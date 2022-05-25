9b0bc0f8628952328e3fddddea9a828dfb45bdd5
Alkesh Vaghela <alkeshgamer@gmail.com>
2022-05-25 14:02:06 +0530

    added android libraries

--- app/src/main/java/com/as1nkr0n8/pullrequestviewer/MainActivityCompose.kt
+++ app/src/main/java/com/as1nkr0n8/pullrequestviewer/MainActivityCompose.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.as1nkr0n8.pullrequestviewer.ui.theme.PullRequestViewerTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivityCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
