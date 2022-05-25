d835835561dd73a5907f81343af6107857400c3c
Alkesh Vaghela <alkeshgamer@gmail.com>
2022-05-25 11:43:20 +0530

    added logging interceptor

--- data/src/main/java/com/as1nkr0n8/data/network/RetrofitClient.kt
+++ data/src/main/java/com/as1nkr0n8/data/network/RetrofitClient.kt
@@ -1,6 +1,8 @@
 package com.as1nkr0n8.data.network
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,9 +19,14 @@ class RetrofitClient {
                 val gsonInstance = GsonBuilder()
                     .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")//2011-04-10T20:09:31Z
                     .create()
+                val okHttpClient = OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    }).build()
                 instance = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create(gsonInstance))
+                    .client(okHttpClient)
                     .build()
                     .create(GitRepoService::class.java)
                 instance!!
